7b8f1e004ee88d418eb752049ea79e1c709f1d51
Miłosz Mikołajczyk <=>
2022-08-27 14:32:38 +0200

    First additions to password recovery system, WIP

--- modules/recovery_email.py
+++ modules/recovery_email.py
@@ -7,6 +7,7 @@ from kivy.app import App
 from kivy.garden.iconfonts import icon
 from kivy.properties import StringProperty, ObjectProperty, NumericProperty
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import FadeTransition, Screen
 from kivy.utils import rgba
 
@@ -27,7 +28,12 @@ class ForgotPasswordScreen(Screen):
 
 
 def sendRecoveryEmail():
-    pass
+    print("SENT")
+
+
+class InfoLabel(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class SendEmailButton(Button):
@@ -35,27 +41,51 @@ class SendEmailButton(Button):
     recoveryEmailBox = ObjectProperty()
     infoLabel = ObjectProperty()
     a = NumericProperty(5)
+    # b = NumericProperty(5)
+    # firstCycle = True
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ids.rectColor = self.color
         self.rectColor = "#0fafff"
 
+    def set_info_label(self):
+        print("znów here hehe" + str(self.a))
+        anim = Animation(a=0, duration=self.a)
+        self.infoLabel.opacity = 1
+        self.infoLabel.color = rgba("08c48c")
+
+        def on_ref_pressed(smth, obj):
+            print("RESEND MESSAGE")
+            sendRecoveryEmail()
+            self.set_info_label()
+
+        def finish_callback(animation, clock):
+            self.infoLabel.text = "[size=24]%s[/size] Recovery E-mail successfully sent! [ref=xd][u]Send again[" \
+                                  "/ref][/u]" \
+                                  % (icon('zmdi-check-circle'))
+            self.infoLabel.bind(on_ref_press=on_ref_pressed)
+
+        anim.bind(on_complete=finish_callback)
+        anim.start(self)
+
     def on_pressed(self):
         self.rectColor = "#0f87ff"
         if checkIsEmailInDatabase(self.recoveryEmailBox.text):
-            anim = Animation(a=0, duration=self.a)
-            self.infoLabel.opacity = 1
-            self.infoLabel.color = rgba("08c48c")
+            self.set_info_label()
+            self.disabled = True
+            self.disabled_color = self.color
+            self.recoveryEmailBox.disabled = True
+            rmAnim = Animation(opacity=0, duration=1.0)
+
+            def actually_remove_widget(instance, obj):
+                self.remove_widget(self)
+
+            rmAnim.bind(on_complete=actually_remove_widget)
+            rmAnim.start(self)
+            globals.hoverEventObjects.pop(1)
             sendRecoveryEmail()
 
-            def finish_callback(animation, clock):
-                self.infoLabel.text = "[size=24]%s[/size] Recovery E-mail successfully sent! [ref=xd][u]Send again[" \
-                                      "/ref][/u]" \
-                                      % (icon('zmdi-check-circle'))
-
-            anim.bind(on_complete=finish_callback)
-            anim.start(self)
         else:
             self.infoLabel.opacity = 1
             self.infoLabel.color = rgba("c92a1e")
