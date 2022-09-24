a54d79a2d50848a8d124bcf05a581448b62da884
szymon <szymonmarciniak22@gmail.com>
2022-09-25 00:38:42 +0200

    Add functions that add cameras

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ import math
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics.context_instructions import Color
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.core.text import LabelBase
+from kivy.uix.button import Button
+from kivy.garden.iconfonts import iconfonts
+from kivy import Config
 
 from utils import CenterOfLine
 
-class SetupScreen(BoxLayout):
+class camera(Button):
+    x1 = NumericProperty()
+    y1 = NumericProperty()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.chose = "None"
+
+    
+class SetupScreen(BoxLayout):
+    floatlayout = ObjectProperty()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        LabelBase.register(name='Lato',
+                           fn_regular='fonts/Lato-Regular.ttf',
+                           fn_bold='fonts/Lato-Bold.ttf')
+        iconfonts.register("default_font", 'fonts/Material-Design-Iconic-Font.ttf', 'fonts/zmd.fontd')
+        Config.set('kivy', 'default_font', 'Lato')
+
+        self.choose = "None"
         with self.canvas:
             Color(200/255,200/255,200/255)
             self.main_room = Line(width=1.5)
@@ -22,17 +42,21 @@ class SetupScreen(BoxLayout):
         self.end_points = []
 
         self.double = True
-        self.rooms = 0
 
         self.rooms_points = []
         self.doors_points = []
+        self.camera_points = []
 
         self.rooms_list = []
         self.door_list = []
-    
-    def create_room_activate(self):
-        self.chose = "new_room"
+        self.cameras_list = []
 
+    def create_room_activate(self):
+        if self.ids["CreateRoomButton"].state == "down":
+            self.choose = "new_room"
+        else:
+            self.choose = "spectate"
+        
     def rooms_refresh(self, *args):
         for id, line in enumerate(self.rooms_list):
             p = len(self.rooms_list) 
@@ -47,12 +71,9 @@ class SetupScreen(BoxLayout):
         self.x1 = self.width * self.kx1
         self.y1 = self.height * self.ky1
 
-        if self.end_points:
-            self.kx2, self.ky2 = self.end_points
-            self.x2 = self.width * self.kx2
-            self.y2 = self.height * self.ky2
-        else:
-            self.x2, self.y2 = self.x1, self.y1
+        self.kx2, self.ky2 = self.end_points
+        self.x2 = self.width * self.kx2
+        self.y2 = self.height * self.ky2
         
         self.main_room.points = self.x1,self.y2,self.x1,self.y1,self.x2,self.y1,self.x2,self.y2,self.x1,self.y2 
         #center_x_top, center_y_top = CenterOfLine((self.kx2, self.kx1),(self.ky2,self.ky1), self.width, self.height)
@@ -60,9 +81,11 @@ class SetupScreen(BoxLayout):
         print(f"Distance: x:{float(math.sqrt((self.kx2-self.kx1)**2)/10) * 1000:.1f}m y:{float(math.sqrt((self.ky2-self.ky1)**2)/10) * 1000:.1f}m")
         #Label(text=f"{float(math.sqrt((self.kx2-self.kx1)**2)/10) * 1000:.1f}", pos_hint={'x': center_x_top, 'y': center_y_top }, color=[0.56, 0.56, 0.24, 1], font_size='20sp')
            
-    
     def create_door_activate(self):
-        self.chose = "create_door"
+        if self.ids["CreateDoorButton"].state == "down":
+            self.choose = "create_door"
+        else:
+            self.choose = "spectate"
     
     def doors_refresh(self, *args):
         for id, door in enumerate(self.door_list):
@@ -74,28 +97,39 @@ class SetupScreen(BoxLayout):
                 door.points = x1, y1, x2, y2
     
     def create_door_function(self, *args):
-
         self.kx1, self.ky1 = self.points
         self.x1 = self.width * self.kx1
         self.y1 = self.height * self.ky1
 
-        if self.end_points:
-            self.kx2, self.ky2 = self.end_points
-            self.x2 = self.width * self.kx2
-            self.y2 = self.height * self.ky2
-        else:
-            self.x2, self.y2 = self.x1, self.y1
+        self.kx2, self.ky2 = self.end_points
+        self.x2 = self.width * self.kx2
+        self.y2 = self.height * self.ky2
         
         self.main_door.points = self.x1, self.y1, self.x2, self.y2
 
+    def add_camera_activate(self):
+        if self.ids["CreateCameraButton"].state == "down":
+            self.choose = "add_camera"
+        else:
+            self.choose = "spectate"
+        
+    def add_camera_function(self, new_camera):
+        self.kx1, self.ky1 = self.points
+        
+        new_camera.x1 = self.kx1
+        new_camera.y1 = self.ky1
+        new_camera.name = "asd"
 
+    def spectate_activation(self):
+        self.choose = "spectate"
+        
     def on_touch_down(self, touch):
         if super(SetupScreen, self).on_touch_down(touch): #If clicking not in create room zone 
             self.up = False
             return False
 
         self.up = True
-        if self.chose == "new_room":
+        if self.choose == "new_room":
             with self.canvas:
                 Color(200/255,200/255,200/255)
                 line = Line(width=1.5)
@@ -107,7 +141,7 @@ class SetupScreen(BoxLayout):
             self.end_points = (x,y)
             self.create_room_function()
 
-        elif self.chose == "create_door":
+        elif self.choose == "create_door":
             with self.canvas:
                 Color(0/255,0/255,0/255)
                 door = Line(width=8, cap="square")
@@ -119,15 +153,25 @@ class SetupScreen(BoxLayout):
             self.end_points = (x,y)
             self.create_door_function()
 
+        elif self.choose == "add_camera":
+            new_camera = camera(size_hint = (0,0))
+            self.cameras_list.append(new_camera)
+            self.floatlayout.add_widget(new_camera)
+            touch.grab(self)
+            x =  touch.pos[0]/ self.floatlayout.width
+            y = touch.pos[1] / self.floatlayout.height
+            self.points = (x, y)
+            self.add_camera_function(new_camera)
+
     def on_touch_move(self, touch):
-        if self.chose == "new_room":
+        if self.choose == "new_room":
             if touch.grab_current is self:
                 x =  touch.pos[0]/ self.width
                 y = touch.pos[1] / self.height
                 self.end_points = (x,y)
                 self.create_room_function()
 
-        elif self.chose == "create_door":
+        elif self.choose == "create_door":
             if touch.grab_current is self:
                 x =  touch.pos[0]/ self.width
                 y = touch.pos[1] / self.height
@@ -140,25 +184,26 @@ class SetupScreen(BoxLayout):
         if not self.up: #If clicking not in create room zone 
             return False 
             
-        if self.chose == "new_room":
+        if self.choose == "new_room":
             self.double = not self.double  
             if self.double:
                 self.rooms_points.append([self.kx1,self.ky1,self.kx2,self.ky2])
-                self.rooms += 1
                 self.main_room.points = 0,0
                 self.rooms_refresh()
         
-        if self.chose == "create_door":
+        if self.choose == "create_door":
             self.double = not self.double  
             if self.double:
                 self.doors_points.append([self.kx1,self.ky1,self.kx2,self.ky2])
-                self.rooms += 1
                 self.main_door.points = 0,0
                 self.doors_refresh()
-        
 
-        
-
+        if self.choose == "add_camera":
+            self.double = not self.double  
+            if self.double:
+                self.camera_points.append([self.kx1,self.ky1])
+                
+                
 class EmployeeSystemApp(App):
     pass
 
