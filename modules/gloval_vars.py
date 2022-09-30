97c0dc62bbb18fb8ccddb4186462cc1bb3c86561
Miłosz Mikołajczyk <=>
2022-09-30 20:41:52 +0200

    Rename file

--- modules/gloval_vars.py
+++ modules/gloval_vars.py
@@ -9,3 +9,4 @@ SECONDARY_COLOR = rgba('#0f87ff')
 DECORATION_COLOR = rgba('#08c48c30')
 DECORATION_COLOR_NOALPHA = rgba('#08c48c')
 ERROR_COLOR = rgba("#c92a1e")
+cameras_names = ['Entrance', 'Ramp #1', 'Ramp #2', 'Rooftop']  # Can be made as a dictionary with ID:Name pair
