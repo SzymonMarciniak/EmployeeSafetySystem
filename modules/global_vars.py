e7cee73ecc7d3ab454162d18b78efb977e1ac422
Miłosz Mikołajczyk <=>
2022-10-12 21:11:13 +0200

    Remove alpha hex format, fix for android

--- modules/global_vars.py
+++ modules/global_vars.py
@@ -6,7 +6,7 @@ choosenWorkplace = None
 BG_COLOR = rgba('#ececec')
 MAIN_COLOR = rgba('#0fafff')
 SECONDARY_COLOR = rgba('#0f87ff')
-DECORATION_COLOR = rgba('#08c48c30')
+DECORATION_COLOR = [0.031, 0.768, 0.549, 0.2]
 DECORATION_COLOR_NOALPHA = rgba('#08c48c')
 ERROR_COLOR = rgba("#c92a1e")
 
