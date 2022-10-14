1b40f5490f53910ee839c688377b370d46e7d02a
szymon <szymonmarciniak22@gmail.com>
2022-10-14 21:31:46 +0200

    Removing unnecessary files

--- utils/metrics.py
+++
@@ -1,5 +0,0 @@
-# Model validation metrics
-def fitness(x):
-    # Model fitness as a weighted combination of metrics
-    w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
-    return (x[:, :4] * w).sum(1)
